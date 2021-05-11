16ec362c2ee396cad187df26eb8c0ea8e4fcebea
이윤경 <62500947+yunkyung-creator@users.noreply.github.com>
2021-05-12 05:05:12 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,19 +1,22 @@
 var Links = {
     setColor:function(color) {
-      var alist = document.querySelectorAll('a');
-      var i = 0;
-      while(i < alist.length) {
-        alist[i].style.color = color;
-        i = i + 1;
-      }
+    //   var alist = document.querySelectorAll('a');
+    //   var i = 0;
+    //   while(i < alist.length) {
+    //     alist[i].style.color = color;
+    //     i = i + 1;
+    //   }
+    $('a').css('color', color);  // 이 웹 페이지의 모든 'a' 태그를 jQuery로 제어하겠다는 뜻
     }
   }
 var Body = {
     setColor:function(color) {
-      document.querySelector('body').style.color = color;  
+      //document.querySelector('body').style.color = color;  
+      $('body').css('color', color);
     },
     setBackgroundColor:function(color) {
-      document.querySelector('body').style.backgroundColor = color;
+      //document.querySelector('body').style.backgroundColor = color;
+      $('body').css('backgroundColor', color);
     }
   }
 function nightDayHandler(self) {
